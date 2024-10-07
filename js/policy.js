18510f030ac1d147f8f8afafd71f40bae7266cb9
oscarivanrs <oscarivan.sanchez@gmail.com>
2024-10-07 15:43:35 +0100

    fix: privacy onload removed

--- js/policy.js
+++ js/policy.js
@@ -1,7 +1,3 @@
 function openPopup() {
     window.open("https://oscarivan.it/policy.html", "myPopup", "width=800,height=600,scrollbars=yes,resizable=yes");
   }
-  
-  window.onload = function() {
-    openPopup();
-  };
